ecad0292cfb62151c10632787157ac591940b808
lei <18264315@qq.com>
2024-04-17 12:21:35 +0800

    新增 api 模块

---
+++ anda-api/src/main/java/com/anda/AndaApiApplication.java
@@ -0,0 +1,13 @@
+package com.anda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndaApiApplication.class, args);
+    }
+
+}
