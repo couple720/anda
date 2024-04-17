e70ab2d1a592d4d7dc8ed24718c894ac099aae54
lei <18264315@qq.com>
2024-04-17 17:53:01 +0800

    引入 element-plus

--- anda-admin-ui/src/App.vue
+++ anda-admin-ui/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <RouterView />
+    <el-config-provider :locale="locale">
+      <RouterView />
+    </el-config-provider>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+const locale = ref(zhCn) 
+</script>
 <style scoped></style>
