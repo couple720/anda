8f83a3442fe79ec089cf9b080c4f60c9460384c2
lei <18264315@qq.com>
2024-04-18 11:42:23 +0800

    env 环境

--- anda-admin-ui/src/App.vue
+++ anda-admin-ui/src/App.vue
@@ -6,8 +6,8 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-const locale = ref(zhCn) 
+const locale = ref(zhCn)
 </script>
 <style scoped></style>
--- anda-admin-ui/src/stores/app.ts
+++ anda-admin-ui/src/stores/app.ts
@@ -2,7 +2,7 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('app', () => {
-  const title = ref('')
+  const title = ref(import.meta.env.VITE_APP_TITLE)
   function setTitle(value: string) {
     title.value = value
   }
