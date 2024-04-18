0c2037e0545f32374b60c5a6fa30cd7ec4677cf5
lei <18264315@qq.com>
2024-04-18 15:22:46 +0800

    引入 Crontab

--- anda-ui/config/process.php
+++ anda-ui/config/process.php
@@ -38,5 +38,8 @@ return [
                 'enable_memory_monitor' => DIRECTORY_SEPARATOR === '/',
             ]
         ]
-    ]
+    ],
+    'task' => [
+        'handler' => process\Task::class
+    ],
 ];
